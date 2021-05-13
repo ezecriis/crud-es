649a4cbba1b022fae1376722236a8f8776f60758
Cris <cr.ledesma@hotmail.com>
2021-05-13 11:52:28 -0300

    App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+import CrudApi from './components/CrudApi';
 import CrudApp from './components/CrudApp';
+import SongSearch from './components/SongSearch';
 import './index.css';
 
 function App() {
   return (
     <>
       <h1>Ejercicios React</h1>
-      <CrudApp />
+      <SongSearch />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
       <hr />
+      <CrudApi />
+      <hr />
+      <CrudApp />
     </>
   );
 }
